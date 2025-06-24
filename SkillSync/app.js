650ce4d40b32b567b6ad9afd59c0552d4bfd7162
ayush13344 <147728663+ayush13344@users.noreply.github.com>
2025-06-24 14:23:40 +0530

    Update app.js

--- SkillSync/app.js
+++ SkillSync/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-// const mongoose = require("mongoose");
+ const mongoose = require("mongoose");
 // const Listing = require("./models/listing.js");
 const path = require("path");
 // const methodOverride = require("method-override");
@@ -10,19 +10,20 @@ const ejsMate = require("ejs-mate");
 // const { listingSchema , reviewSchema } = require("./schema.js");
 // const Review = require("./models/review.js")
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
-// main()
-//   .then(() => {
-//     console.log("connected to DB");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
 
-// async function main() {
-//   await mongoose.connect(MONGO_URL);
-// }
+main()
+   .then(() => {
+     console.log("connected to DB");
+   })
+   .catch((err) => {
+     console.log(err);
+   });
+
+ async function main() {
+   await mongoose.connect(MONGO_URL);
+ }
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
